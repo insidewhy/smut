a84aff610a79b14d22df115ec0dbbdd53b03ce91
James Pike <jpike@chilon.net>
2014-11-23 05:17:15 +0000

    Ported to ember-cling style mostly.

--- Plumbing.js
+++ Plumbing.js
@@ -3,19 +3,27 @@ var glob      = require('plumber-glob')
 var traceur   = require('plumber-traceur')
 var concat    = require('plumber-concat')
 var write     = require('plumber-write')
+var ember_tmplts = require('plumber-ember-template-compiler')
+
+var getModulePath = function(path) {
+  return path.replace(/[^/]+/, 'smut');
+}
 
 module.exports = function(pipelines) {
   var sources = glob.within('app')
 
   pipelines['compile:js'] = [
     sources('**/*.js'),
-    traceur.toAmd({
-      getModulePath: function(path) {
-        return path.replace(/[^/]+/, 'smut');
-      }
-    }),
+    traceur.toAmd({ getModulePath: getModulePath }),
     glob('bootstrap.js'),
     concat('smut'),
+    write('dist/assets')
+  ]
+
+  pipelines['compile:hbs'] = [
+    sources('**/*.hbs'),
+    ember_tmplts({ getModulePath: getModulePath }),
+    concat('templates'),
     write('dist/assets')
   ]
 }
